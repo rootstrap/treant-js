1d34e98ae37bd02419e2dedc8e7398860fcfd214
Emanuel Galbarini <emanuel@rootstrap.com>
2019-08-28 14:36:15 -0300

    Adding raphael and jquery imports

--- Treant.js
+++ Treant.js
@@ -17,6 +17,9 @@
  * Dave Goodchild, https://github.com/dlgoodchild
  */
 
+const Raphael = require('raphael');
+const $ = require('jquery');
+
 ;( function() {
     // Polyfill for IE to use startsWith
     if (!String.prototype.startsWith) {
@@ -876,7 +879,7 @@
             } // else this.CONFIG.scrollbar == 'None'
 
             return this;
-        },        
+        },
         /**
          * @param {TreeNode} treeNode
          * @param {boolean} hidePoint
@@ -1854,9 +1857,9 @@
                 if (key.startsWith("data-")) {
                     node.setAttribute(key, this.text[key]);
                 } else {
-                    
+
                     var textElement = document.createElement(this.text[key].href ? 'a' : 'p');
-                    
+
                     // make an <a> element if required
                     if (this.text[key].href) {
                         textElement.href = this.text[key].href;
@@ -1864,14 +1867,14 @@
                             textElement.target = this.text[key].target;
                         }
                     }
-                    
+
                     textElement.className =  "node-"+key;
                     textElement.appendChild(document.createTextNode(
                         this.text[key].val ? this.text[key].val :
                         this.text[key] instanceof Object ? "'val' param missing!" : this.text[key]
                     )
                     );
-                    
+
                     node.appendChild(textElement);
                 }
             }
